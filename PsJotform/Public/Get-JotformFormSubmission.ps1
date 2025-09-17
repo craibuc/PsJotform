e690c606289022092cc6249fa54b31b28a0d925e
Craig Buchanan <craig.buchanan@cogniza.com>
2025-09-17 10:43:36 -0500

    adding paging adding sorting

--- PsJotform/Public/Get-JotformFormSubmission.ps1
+++ PsJotform/Public/Get-JotformFormSubmission.ps1
@@ -10,17 +10,41 @@ function Get-JotformFormSubmission {
         [Parameter(Mandatory)]
         [string]$Subdomain,
 
-        [string]$FormId
+        [string]$FormId,
+
+        [ValidateSet('id','username','title','status','created_at','updated_at','new','count','slug')]
+        [string]$OrderBy
     )
 
-    $BaseUri = "https://$Subdomain.jotform.com/API"
+    $BaseUri = "https://$Subdomain.jotform.com/API/form/$FormId/submissions"
+    $Offset = 0
 
-    $Uri = '{0}/form/{1}/submissions' -f $BaseUri, $FormId
+    do {
 
-    $Response = Invoke-WebRequest -Method Get -Uri $Uri -Headers @{APIKEY = $ApiKey} 
+        # offset; orderby
+        $QueryParams = @{}
 
-    if ($Response.Content) {
-        ($Response.Content | ConvertFrom-Json -Depth 10).content
-    }
+        $QueryParams.offset = $Offset
+        if ($OrderBy) { $QueryParams.orderby = $OrderBy }
+
+        $QueryString = ($QueryParams.GetEnumerator() | ForEach-Object { "$($_.Key)=$($_.Value)" }) -join '&'
+
+        $Uri = $QueryString ? "$BaseUri`?$QueryString" : $BaseUri
+        Write-Debug "Uri: $Uri"
+
+        $Response = Invoke-WebRequest -Method Get -Uri $Uri -Headers @{APIKEY = $ApiKey}
+
+        if ($Response.Content) {
+            # convert JSON to PsCustomObject
+            $Content = $Response.Content | ConvertFrom-Json -Depth 10
+            
+            # calculate new offset
+            $Offset = $Offset += [int]$Content.resultSet.limit
+
+            # return the records
+            $Content.content
+        }
+
+    } while ($Content.resultSet.count -gt 0)
 
 }
